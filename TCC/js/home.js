94786fe53db234be15002bd4c404dcbe144af8e7
Lucas <126921719+LN05@users.noreply.github.com>
2023-04-08 13:23:47 -0300

    Add files via upload

--- TCC/js/home.js
+++ TCC/js/home.js
@@ -6,15 +6,3 @@ function toggleMenu(){
         menuMobile.className = "menu-mobile-active"
     }
 }
-
-$('.agendamento').click(function(){
-    $('.sidebar ul .itemAgendamento').toggleClass('mostra');
-});
-
-$('.agendamento').mouseover(function(){
-    $('.sidebar ul .seta1').toggleClass('girar');
-});
-
-$('.agendamento').mouseout(function(){
-    $('.sidebar ul .seta1').toggleClass('girar');
-});
