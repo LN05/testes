90a946ac43fd8a045091460f5ef469b99ff11cc0
KaikyRMendes <107228751+KaikyRMendes@users.noreply.github.com>
2023-04-13 20:34:04 -0300

    Update home.js

--- TCC/js/home.js
+++ TCC/js/home.js
@@ -32,3 +32,41 @@ function mostrar_modalDisponibilidade() {
     myModal.show();
 }
 
+/* MODAL DA TELA ADM*/
+function mostrar_modalAdmNovo() {
+    const elemento = document.getElementById('modal_admNovo')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalAdmEditar() {
+    const elemento = document.getElementById('modal_admEditar')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalAdmEditarFuncionario() {
+    const elemento = document.getElementById('modal_admEditarFuncionario')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalAdmExcluirFuncionario() {
+    const elemento = document.getElementById('modal_admExcluirFuncionario')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalAdmEditarCliente() {
+    const elemento = document.getElementById('modal_admEditarCliente')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalAdmExcluirCliente() {
+    const elemento = document.getElementById('modal_admExcluirCliente')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+
