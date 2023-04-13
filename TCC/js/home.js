29852ffe4a006d6735f0a89321be7a1010c4f248
KaikyRMendes <107228751+KaikyRMendes@users.noreply.github.com>
2023-04-12 23:56:54 -0300

    Update home.js

--- TCC/js/home.js
+++ TCC/js/home.js
@@ -1,8 +1,34 @@
-function toggleMenu(){
+function toggleMenu() {
     const menuMobile = document.getElementById("menu-mobile")
-    if(menuMobile.className === "menu-mobile-active"){
+    if (menuMobile.className === "menu-mobile-active") {
         menuMobile.className = "menu-mobile"
-    } else{
+    } else {
         menuMobile.className = "menu-mobile-active"
     }
 }
+
+/*   MODAL  */
+function mostrar_modalAgendamento() {
+    const elemento = document.getElementById('modal_novoAgendamento')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalEditar() {
+    const elemento = document.getElementById('modal_editarAgendamento')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalExcluir() {
+    const elemento = document.getElementById('modal_excluirAgendamento')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
+function mostrar_modalDisponibilidade() {
+    const elemento = document.getElementById('modal_addDisponibilidade')
+    const myModal = new bootstrap.Modal(elemento);
+    myModal.show();
+}
+
